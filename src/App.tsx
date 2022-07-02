5762fd4d4deceeb502063552ca159fcb90623892
Tian You <tian.you@outlook.com>
2022-07-02 16:22:46 +0200

    Update the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import theme from "./theme";
+
+// 1. import `ChakraProvider` component
+import {
+    ChakraProvider,
+    Heading,
+    Text,
+    Button,
+    useColorMode,
+} from "@chakra-ui/react";
+
+function Example() {
+    const { colorMode, toggleColorMode } = useColorMode();
+    return (
+        <header>
+            <Button onClick={toggleColorMode}>
+                Toggle {colorMode === "light" ? "Dark" : "Light"}
+            </Button>
+        </header>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // 2. Wrap ChakraProvider at the root of your app
+    return (
+        <ChakraProvider theme={theme}>
+            <div className="App">
+            <Heading as="h1" size="xl" noOfLines={1} className="header">
+                    Hi!
+                </Heading>
+
+                <div className="AppContent">
+                    <Text> Not sure how you landed on this page, but as you can see, it is still work in progress. </Text>
+                    <Text> I am hoping to finish building this personal website by the end of this summer. </Text>
+                    <Text paddingBottom={'25pt'}> If you like, you can toggle the button below to change the colour mode. </Text>
+                    <Example></Example>
+                    
+                </div>
+            </div>
+        </ChakraProvider>
+    );
 }
 
 export default App;
